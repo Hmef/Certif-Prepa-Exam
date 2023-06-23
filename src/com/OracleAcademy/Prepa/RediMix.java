89f53c2732a844641ac5747cbe50c3b939588167
Hmef <hmeftaah@gmail.com>
2023-06-23 23:53:57 +0200

    Certif Oracle Academy Prepa - abstract - heritage

---
+++ src/com/OracleAcademy/Prepa/RediMix.java
@@ -0,0 +1,13 @@
+package com.OracleAcademy.Prepa;
+
+public class RediMix extends Concrete {
+
+	public RediMix() {
+		// TODO Auto-generated constructor stub
+		System.out.println("r ");
+	}
+	
+	public static void main(String[] args) {
+		new RediMix();
+	}
+}
