9850b7502cd20f96ae983100b764efb6f231939b
Hmef <hmeftaah@gmail.com>
2023-06-23 23:59:32 +0200

    Certif Oracle Academy Prepa - Runnable Interface - Thread

--- src/com/OracleAcademy/Prepa/secondTest/Mosey.java
+++ src/com/OracleAcademy/Prepa/secondTest/Mosey.java
@@ -8,7 +8,7 @@ public class Mosey implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 1000; i++) {
-			System.out.println(Thread.currentThread().getId() + "-" + i + "");
+			System.out.println(Thread.currentThread().getId() + "-" + i + "" );
 		}
 	}
 
