6db4c5f5eeeefbad65f1c261f4ca353a55cb620c
Hmef <hmeftaah@gmail.com>
2023-07-13 23:50:13 +0200

    pancakeMethod - Before and after method's call - List<String> list2 = new PancakeClass().pancakeMethod(list1) - list3.addAll(list2)

--- src/com/OCP/ListInterface/PancakeClass.java
+++ src/com/OCP/ListInterface/PancakeClass.java
@@ -7,23 +7,28 @@ public class PancakeClass implements Pancake {
 
 	public static void main(String[] args) {
 		
-		List<String> list1 = new ArrayList<String>();
+		List<String> list1 = new ArrayList<>();
 		list1.add("3");
 		list1.add("5");
 		list1.add("7");
 		
 		System.out.println(" Before method's call ==> " + list1 );
-		
+
 		List<String> list2 = new PancakeClass().pancakeMethod(list1);
 		list2.add("1");
+		
 		System.out.println(" After the method's call ==> " + list1);
 		System.out.println(" list2 ==> " + list2);
+		
+		List<String> list3 = new ArrayList<>();
+		list3.addAll(list2);
+		System.out.println(" list3 ==> " + list3);
 	}
 	
 	@Override
 	public List<String> pancakeMethod(List<String> list) {
 		// TODO Auto-generated method stub
-		list.add("9");
+		list.add("99999");
 		System.out.println(" Content of pancakeMethod method ==> " + list);
 		return list;
 	}
