bfefec1df04430d52afa4d4d900d817ba0cb3dca
Hmef <hmeftaah@gmail.com>
2023-07-15 01:56:43 +0200

    NumberFormat - RoundingMode.HALF_UP - GroupingUsed - MaximumIntegerDigits < MinimumIntegerDigits

--- src/com/OCP/NumberFormat/Parse/DecimalFormatTest.java
+++ src/com/OCP/NumberFormat/Parse/DecimalFormatTest.java
@@ -13,8 +13,9 @@ public class DecimalFormatTest {
 		 double PI = 3.1415;
 		 System.out.printf(" Value with 3 digits after decimal point %.3f %n", PI);
 		 
+		 // DecimalFormat 
 		 DecimalFormat df = new DecimalFormat("###.###");
-		 System.out.println(df.format(PI));
+		 System.out.println(df.format(PI)); // 3,142
 		 
 		 // NumberFormat ',' for Locale.FRENCH  
 		 NumberFormat.getInstance(Locale.FRENCH).format(1.235d); // 1,235
@@ -24,17 +25,59 @@ public class DecimalFormatTest {
 		 NumberFormat instance = NumberFormat.getNumberInstance();
 		 //NumberFormat instance = NumberFormat.getInstance();
 		 instance.format(1.1111);
-		 instance.setMaximumFractionDigits(4);
+		 instance.setMaximumFractionDigits(1);
 		 instance.setMinimumFractionDigits(2);
 		 instance.setGroupingUsed(false);
 		 instance.setRoundingMode(RoundingMode.HALF_UP);
 		 
-		 System.out.println(" instance : " + instance.format(1.643322));
+		 System.out.println("  ");
+		 System.out.println(" == RoudingMode.HALF-UP == ");
+		 
+		 System.out.println(instance.format(1.643322));
 		 System.out.println(instance.format(1234.665D));
+		 
+		 
+		 final NumberFormat instanceInteger = NumberFormat.getNumberInstance();
+		 instanceInteger.setMaximumIntegerDigits(6);
+		 instanceInteger.setMinimumIntegerDigits(4);
+		 System.out.println("  ");
+		 instanceInteger.setGroupingUsed(true); // GroupingUsed(true)
+		 System.out.println(" == GroupingUsed(true) == ");
+		 String ss = instanceInteger.format(12345D);
+		 String sss = instanceInteger.format(1D);
+		 System.out.println(ss);
+		 System.out.println(sss);
+		 
+		 instanceInteger.setGroupingUsed(false);  // GroupingUsed(false)
+		 System.out.println("  ");
+		 System.out.println(" == GroupingUsed(false) == ");
+		 
+		 String ss2 = instanceInteger.format(12345D);
+		 String sss2 = instanceInteger.format(1D);
+		 System.out.println(ss2);
+		 System.out.println(sss2);
+
+		 // MaximumIntegerDigits < MinimumIntegerDigits 
+		 instanceInteger.setMaximumIntegerDigits(1);
+		 String ss3 = instanceInteger.format(12345D);
+		 String sss3 = instanceInteger.format(1D);
+
+		 System.out.println("  ");
+		 System.out.println(" == MaximumIntegerDigits < MinimumIntegerDigits == ");
+		 System.out.println(ss3);
+		 System.out.println(sss3);
 		 
 		 //String s = instance.format(1.543366744);
 		 //System.out.println(" s : " + s);
+		 /*
+		 //DecimalFormat applPattern
+		 NumberFormat instancePattern = NumberFormat.getNumberInstance();
+		 ((DecimalFormat) instancePattern).applyPattern("###.00 €");
+		 String ssPattern = instancePattern.format(1.65432);
+		 System.out.println("  ");
+		 System.out.println(" DecimalFormat - applyPattern");
+		 System.out.println(ssPattern);
 		 
-			
+		 */
 	}
 }
