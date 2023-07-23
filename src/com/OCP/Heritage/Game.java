04e5933567c2b6099510f2afb636f7b32839f487
Hmef <hmeftaah@gmail.com>
2023-07-23 23:59:23 +0200

    static - Thread - Runnable Interface

---
+++ src/com/OCP/Heritage/Game.java
@@ -0,0 +1,9 @@
+package com.OCP.Heritage;
+
+public class Game {
+
+	static String s = "-";
+	String s2 = "s2";
+	
+	Game(String arg){ s += arg; }
+}
---
+++ src/com/OCP/Heritage/Go.java
@@ -0,0 +1,10 @@
+package com.OCP.Heritage;
+
+public class Go extends Game{
+
+	Go(String arg) {
+		super(arg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
