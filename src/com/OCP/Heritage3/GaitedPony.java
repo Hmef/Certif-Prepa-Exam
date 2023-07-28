a82f7ba3a29c041ab4d2a0ef777cece0e88c15b9
Hmef <hmeftaah@gmail.com>
2023-07-28 23:57:08 +0200

    Heritage - static attribut - hands of Horse class - GaitedPony class - print getSize(s) and hands value

--- src/com/OCP/Heritage3/GaitedPony.java
+++ src/com/OCP/Heritage3/GaitedPony.java
@@ -2,13 +2,13 @@ package com.OCP.Heritage3;
 
 public class GaitedPony extends Horse {
 
-	//static String hands = "14";
+	static String hands = "14";
 	
 	public static void main(String[] args) {
 		
 		String hands = "13.2";
-		String result = new GaitedPony().getSize(hands); // getSize(hands) ==> return hands of Horse Class 
-		System.out.println(" " + result);
+		String result = new GaitedPony().getSize(hands); // hands == s ==> getSize(s) and print hands = "14"
+		System.out.println(" " + result); // hands : 13.2 14
 	}
 	
 	String getSize(String s) {
