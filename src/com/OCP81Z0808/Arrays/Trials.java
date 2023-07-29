bae113d23511d0493b5f279ee47694499d61f664
Hmef <hmeftaah@gmail.com>
2023-07-29 23:52:45 +0200

    initialize the array - find and store the minimum value - print thee minimum value - randomize() - findMinimum() - Math.random() - Math.min()

--- src/com/OCP81Z0808/Arrays/Trials.java
+++ src/com/OCP81Z0808/Arrays/Trials.java
@@ -4,17 +4,34 @@ public class Trials {
 
 	public static void main(String[] args) {
 		
+		double storeMinimum[] = new double[5];
+		double trialArray[] = new double[15];
+		
+		for(int i = 0; i<storeMinimum.length; ++i) {
+			
+			//initialize the Array
+			randomize(trialArray); 
+			
+			// find and store the minimum value 
+			storeMinimum[i] = findMinimum(trialArray); 
+		}
+		
+		// print the minimum value
+		for(double minValue : storeMinimum)
+			System.out.println( minValue);
+		
 		
 	}
 	
 	public static void randomize(double[] valArray) {
 		
 		for(int i=0; i<valArray.length; ++i) {
-			valArray[i] = Math.random() * 100.0;
+			valArray[i] = Math.random() * 10.0;  //100.0
+			//System.out.println(" valArray 1: " + valArray[i]);
 		}
 	}
 	
-	public static double findMinimum(double valArray[]) {
+	public static double findMinimum(double[] valArray) {
 		
 		double minValue = valArray[0];
 		for(int i = 0; i<valArray.length; ++i) {
