2813e3f5905a76ca290087721dd95922b8030681
Hmef <hmeftaah@gmail.com>
2023-08-02 23:40:55 +0200

    Ambiguous overload - aMethod(long,int) - call aMethod(9,10) - 2 matches ( integer can be upcasted to both long and Integer )

--- src/com/OCP81Z0809/Overloading/AmbiguousOverload.java
+++ src/com/OCP81Z0809/Overloading/AmbiguousOverload.java
@@ -16,6 +16,25 @@ public class AmbiguousOverload {
 	
 	public static void main(String[] args) {
 		
-		aMethod(9, 10);
+		//aMethod(9, 10); // Error Ambiguous Overload - there are two matches ( long and Integer ) 
+		
 	}
 }
+
+
+/*
+ * 
+ * 
+			
+			Why did this call become an “ambiguous” call? 
+			The constants 9 and 10 are ints. 
+			There are two aMethod definitions: one is aMethod(long, int) and another is aMethod(int, long)
+			So there is no exact match for the call aMethod(int, int).
+			An integer can be implicitly upcasted to both long as well as Integer
+			
+			Which one will the compiler choose? 
+			Since there are TWO matches 
+			the compiler complains with an ERROR that the call is ambiguous. 
+ * 
+ * 
+ */
