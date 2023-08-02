7f7ea92a97eddf66f859fd4ff1ea2575c7034b23
Hmef <hmeftaah@gmail.com>
2023-08-02 23:33:35 +0200

    Ambiguous overload - aMethod(long,int) - call aMethod(9,10) - 2 matches ( integer can be upcasted to both long and Integer )

--- src/com/OCP81Z0809/Overloading/OverloadingError.java
+++ src/com/OCP81Z0809/Overloading/OverloadingError.java
@@ -6,8 +6,11 @@ public class OverloadingError {
 	public static void aMethod(byte val) { System.out.println("byte");}
 	public static void aMethod(short val) { System.out.println("short");}
 	
+	// solution 
+	public static void aMethod(int val) { System.out.println("int");}
+	
 	public static void main(String[] args) {
 		
-		aMethod(9);
+		aMethod(9); // closet match by upcast (cast byte to int)
 	}
 }
