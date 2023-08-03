079df59436ca839fede6e080de51635b77cf0344
Hmef <hmeftaah@gmail.com>
2023-08-03 23:16:53 +0200

    Heritage - Object <- Number <- Byte - Integer - Float - Double (the derived classes) - with comments

--- src/com/OCP81Z0809/Heritage/TestNumber.java
+++ src/com/OCP81Z0809/Heritage/TestNumber.java
@@ -14,13 +14,30 @@ public class TestNumber {
 	
 	public static void main(String[] args) {
 		
+		// create a Number array
 		Number []nums = new Number[4];
 		
+		// assign derived class objects
 		nums[0] = new Byte((byte)10);
 		nums[1] = new Integer(10);
-		nums[2] = new Float(10.0f); // F 
+		nums[2] = new Float(10.0f); 
 		nums[3] = new Double(10.0f);
 		
+		// pass the Number array to sum and print it 
 		System.out.println(" The sum is : " + sum(nums));
 	}
 }
+
+/*
+ * 
+			A Number Reference can hold any of its derived type objects 
+ * 
+ 			the nums array holds objects of type Byte, Integer, Float and Double
+ * 
+ * 
+  			Why using double as the return type of the sum method ??
+			the double type can hold a largest range of value 
+ * 
+ * 
+ */
+
