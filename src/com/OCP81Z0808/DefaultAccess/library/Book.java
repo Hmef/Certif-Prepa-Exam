21c3e07c95db9e67cb56574ff6eb4ccb1c892aca
Hmef <hmeftaah@gmail.com>
2023-08-07 19:55:28 +0200

    defaut access (package acces) - library package

---
+++ src/com/OCP81Z0808/DefaultAccess/library/Book.java
@@ -0,0 +1,11 @@
+package com.OCP81Z0808.DefaultAccess.library;
+
+public class Book {
+
+	int issueCount;
+	
+	void issueHistory() {
+		
+	}
+	
+}
---
+++ src/com/OCP81Z0808/DefaultAccess/library/CourseBook.java
@@ -0,0 +1,12 @@
+package com.OCP81Z0808.DefaultAccess.library;
+
+public class CourseBook extends Book {
+
+	
+	CourseBook(){
+		
+		int c = issueCount;
+		
+		issueHistory();
+	}
+}
