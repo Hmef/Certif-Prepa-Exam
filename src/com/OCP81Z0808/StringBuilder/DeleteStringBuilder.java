d20f184a3bd513c0b4fe38d7e48b5f14c1513aaa
Hmef <hmeftaah@gmail.com>
2023-08-10 23:09:03 +0200

    Exam TIP - delete() - delete(2,4) ==> Excluding 4 - deleteCharAt() - trim() not defined by StringBuilder class - StringBuilder

---
+++ src/com/OCP81Z0808/StringBuilder/DeleteStringBuilder.java
@@ -0,0 +1,22 @@
+package com.OCP81Z0808.StringBuilder;
+
+public class DeleteStringBuilder {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb1 = new StringBuilder("0123456");
+		sb1.delete(2, 4); // Exam TIP ( removing characters at position starting from 2 to 4 - Excluding 4 )
+		System.out.println(sb1);
+		
+		StringBuilder sb2 = new StringBuilder("0123456");
+		sb2.deleteCharAt(2);
+		System.out.println(sb2);
+	}
+	
+	// *********** Exam TIP ***********
+	
+	// Combinations of the deleteCharAt() and insert() methods can be quite confusing !! 
+	
+	// trim() : the class StringBuilder doesn't define the method trim() 
+	
+}
