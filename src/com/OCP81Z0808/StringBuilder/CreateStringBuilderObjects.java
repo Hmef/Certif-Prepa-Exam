c3c8a1ab5da7942ef73a37b719bede8aa36f88c4
Hmef <hmeftaah@gmail.com>
2023-08-10 22:55:31 +0200

    Exam TIP - insert() String Builder - insert() Vs append() - insert multiple characters

--- src/com/OCP81Z0808/StringBuilder/CreateStringBuilderObjects.java
+++ src/com/OCP81Z0808/StringBuilder/CreateStringBuilderObjects.java
@@ -9,9 +9,12 @@ public class CreateStringBuilderObjects {
 		StringBuilder sb2 = new StringBuilder(sb1);  
 		StringBuilder sb3 = new StringBuilder(50);
 		StringBuilder sb4 = new  StringBuilder("test Stringbuilder");  
+		
+		// ********* Exam TIP *********
+		
+
 	}
 }
-
 
 
 /*
---
+++ src/com/OCP81Z0808/StringBuilder/InsertStringBuilder.java
@@ -0,0 +1,18 @@
+package com.OCP81Z0808.StringBuilder;
+
+public class InsertStringBuilder {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb1 = new StringBuilder("jaa");
+		sb1.insert(2, "v");
+		System.out.println(sb1);
+		
+		// ********* Exam TIP *********
+		StringBuilder sb2 = new StringBuilder("123");
+		char[] name = {'j','a','v','a'};
+		sb2.insert(1, name, 1, 3); // insert multiple characters
+		System.out.println(sb2);
+		
+	}
+}
