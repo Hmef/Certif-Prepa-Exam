6a03e45fc8a8c678be53fdb1df8d815f282d0817
Hmef <hmeftaah@gmail.com>
2023-08-20 23:46:55 +0200

    Exam TIP - LocalDate.of() - atTime()

--- src/com/OCP81Z0808/LocalDate/Main.java
+++ src/com/OCP81Z0808/LocalDate/Main.java
@@ -1,12 +1,13 @@
 package com.OCP81Z0808.LocalDate;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		LocalDate date = LocalDate.parse("2020-08-30");
+		LocalDate date = LocalDate.parse("2020-08-30"); 
 		
 		System.out.println(date.getDayOfMonth());
 		System.out.println(date.getDayOfWeek());
@@ -14,6 +15,20 @@ public class Main {
 		System.out.println(date.getMonth());
 		System.out.println(date.getMonthValue());
 		System.out.println(date.getYear());
+		
+		// 
+		
+		LocalDate interviewDate = LocalDate.of(2016,02,28); // LocalDate.of()
+		
+		System.out.println(interviewDate.atTime(16, 30));
+		System.out.println(interviewDate.atTime(16, 30, 20));
+		System.out.println(interviewDate.atTime(16, 30, 20, 300));
+		System.out.println(interviewDate.atTime(LocalTime.of(16, 30)));
+		
+		/*
+		 * Exam TIP 
+		 */
+		System.out.println(interviewDate.atTime(LocalTime.of(11111, 30))); // throw DateTimeException
 	}
 
 }
