165314e2cb3f88f22a2871d6cd2bc01b9f336ebb
Hmef <hmeftaah@gmail.com>
2023-09-14 22:30:55 +0200

    Sorting - Arrays

---
+++ src/com/OCP17/Array/Sorting/Main.java
@@ -0,0 +1,17 @@
+package com.OCP17.Array.Sorting;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {3, 2, 7, 4};
+		
+		Arrays.sort(numbers);
+		
+		for(int i=0; i<numbers.length; i++)
+			System.out.println(numbers[i]);
+		
+	}
+}
