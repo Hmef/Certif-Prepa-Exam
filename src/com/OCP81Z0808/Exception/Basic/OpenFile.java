02d076d32425b46d264b232be2db431e60b7bcae
Hmef <hmeftaah@gmail.com>
2023-09-15 22:12:37 +0200

    FileNotFoundException - OCP8-1Z0808

---
+++ src/com/OCP81Z0808/Exception/Basic/OpenFile.java
@@ -0,0 +1,26 @@
+package com.OCP81Z0808.Exception.Basic;
+
+import java.io.FileInputStream;
+
+public class OpenFile {
+
+	public static void main(String[] args) {
+		
+		//FileInputStream fls = new FileInputStream("file.txt");
+		
+		/*
+		 
+		 try {
+			FileInputStream fls = new FileInputStream("file.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		 */
+		
+		/*
+		 * ==> FileNotFoundException
+		 */
+	}
+}
