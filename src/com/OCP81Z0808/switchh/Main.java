78c8a9be03c68a68450e25c34f73c1e488884f29
Hmef <hmeftaah@gmail.com>
2023-09-20 23:24:25 +0200

    switch - case - break - default - OCP 8 1Z0808

---
+++ src/com/OCP81Z0808/switchh/Main.java
@@ -0,0 +1,21 @@
+package com.OCP81Z0808.switchh;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int marks = 20;
+		switch (marks) {
+			case 10: System.out.println("10");
+				break;
+			case 20 : System.out.println("20");
+				break;
+			case 30 : System.out.println("30");
+				break;
+			default: System.out.println("default");
+				break;
+
+		}
+
+	}
+}
