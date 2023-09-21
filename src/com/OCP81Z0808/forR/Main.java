c3cdc39e1cd22d1fb365ee4ecf009ca7a40414bb
Hmef <hmeftaah@gmail.com>
2023-09-21 23:53:38 +0200

    for - StringBuilder - OCP8-1Z0808

---
+++ src/com/OCP81Z0808/forR/Main.java
@@ -0,0 +1,30 @@
+package com.OCP81Z0808.forR;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int total = 0;
+		int primeNums[] = {2, 3, 7, 11};
+		
+		for(int num : primeNums) {
+			total += num;
+			System.out.println(num);
+		}
+
+		System.out.println(total);
+		
+		/*
+		 * StringBuilder
+		 */
+		
+		StringBuilder myArr[] = {
+				new StringBuilder("Java"), 
+				new StringBuilder("Oracle")
+		};
+		
+		for(StringBuilder val : myArr)
+			System.out.println(val);
+	}
+
+}
