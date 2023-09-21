fb52550130adca17557879d24370495de79c7b68
Hmef <hmeftaah@gmail.com>
2023-09-21 23:59:54 +0200

    Exam TIP - for - StringBuilder - OCP8-1Z0808

--- src/com/OCP81Z0808/forR/Main.java
+++ src/com/OCP81Z0808/forR/Main.java
@@ -17,12 +17,16 @@ public class Main {
 		/*
 		 * StringBuilder
 		 */
-		
+		System.out.println(" ");
 		StringBuilder myArr[] = {
 				new StringBuilder("Java"), 
 				new StringBuilder("Oracle")
 		};
 		
+		for(StringBuilder val : myArr)
+			System.out.println(val);
+		for(StringBuilder val : myArr)
+			val.append("Loop");
 		for(StringBuilder val : myArr)
 			System.out.println(val);
 	}
