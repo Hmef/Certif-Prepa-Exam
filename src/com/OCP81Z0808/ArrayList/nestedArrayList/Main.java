06467277a7c050e232b9f0558515a8d9124831c3
Hmef <hmeftaah@gmail.com>
2023-09-21 23:36:20 +0200

    ArrayList - nested ArrayList - ArrayList<ArrayList<String>> nestedArrayList = new ArrayList<ArrayList<String>>() - OCP8-1Z0808

---
+++ src/com/OCP81Z0808/ArrayList/nestedArrayList/Main.java
@@ -0,0 +1,31 @@
+package com.OCP81Z0808.ArrayList.nestedArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> exams = new ArrayList<String>();
+		exams.add("Java");
+		exams.add("Oracle");
+		
+		ArrayList<String> levels = new ArrayList<String>();
+		levels.add("Basic");
+		levels.add("Advanced");
+		
+		ArrayList<String> grades = new ArrayList<String>();
+		grades.add("Pass");
+		grades.add("Fails");
+		
+		/*
+		 * Nested ArrayList
+		 */
+		
+		ArrayList<ArrayList<String>> nestedArrayList = new ArrayList<ArrayList<String>>();
+		nestedArrayList.add(exams);
+		nestedArrayList.add(levels);
+		nestedArrayList.add(grades);
+	}
+
+}
