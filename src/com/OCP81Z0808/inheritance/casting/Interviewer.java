b46d79a270fb604eb5683e6b9ebce8c3b467c7d7
Hmef <hmeftaah@gmail.com>
2023-10-07 23:40:19 +0200

    Inheritance - Interviewer Interface - abstract - static - default - OCP81Z0808

---
+++ src/com/OCP81Z0808/inheritance/casting/Interviewer.java
@@ -0,0 +1,25 @@
+package com.OCP81Z0808.inheritance.casting;
+
+import java.time.LocalDateTime;
+
+public interface Interviewer {
+
+	public abstract void conductInterview(); // an abstract method is defined without a method body
+	public default void submitInterviewStatus() {
+		System.out.println("Accept");
+	}
+	
+	/*
+	  
+	  public default void submitInterviewStatus() { } // Empty method body != abstract method 
+	  
+	  ( the return type is void ) 
+	
+	 */
+	
+	public static void bookConferenceRoom(LocalDateTime dateTime, int duration) {
+		
+		System.out.println("Interviw scheduled on: " + dateTime);
+		System.out.println(" Book conference room for : " + duration + "hrs");
+	}
+}
