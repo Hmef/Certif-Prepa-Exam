821f61f4f5da4e505a79c8afdb744b1a96ddd267
Hmef <hmeftaah@gmail.com>
2023-10-13 23:38:30 +0200

    Question Time - Chap 2 - Abstract Class extends CONCRETE class - OCP81Z0809

---
+++ src/com/OCP81Z0809/QuestionTime/Chap2/Base.java
@@ -0,0 +1,8 @@
+package com.OCP81Z0809.QuestionTime.Chap2;
+
+ class Base {
+
+	public void print() {
+		System.out.println("Base:print");
+	}
+}
---
+++ src/com/OCP81Z0809/QuestionTime/Chap2/Test.java
@@ -0,0 +1,20 @@
+package com.OCP81Z0809.QuestionTime.Chap2;
+
+abstract class Test extends Base {
+
+	
+	
+	public static void main(String[] args) {
+		
+		Base obj = new Base();
+		obj.print();
+	}
+	
+	
+}
+
+/*
+ *  Abstract class can extends from a CONCRETE class 
+ * 
+ */
+
