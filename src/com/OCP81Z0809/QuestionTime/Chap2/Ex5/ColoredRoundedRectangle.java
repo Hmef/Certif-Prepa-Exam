c6699da89a07444b8b3be7dfbb1c76d223d94197
Hmef <hmeftaah@gmail.com>
2023-10-17 21:36:27 +0200

    Question Time - Chap2 - EX5 - Compilation Error ==> Cannot Extend Two Classes - Java doesn't support a multiple class Inheritance - It is Acceptable for a Class to be Empty - OCP81Z0809

---
+++ src/com/OCP81Z0809/QuestionTime/Chap2/Ex5/ColoredRoundedRectangle.java
@@ -0,0 +1,13 @@
+package com.OCP81Z0809.QuestionTime.Chap2.Ex5;
+
+
+// Compilatio Error ==> Cannot extend two classes 
+
+//public class ColoredRoundedRectangle extends ColoredRectangle, RoundedRectangle {
+
+public class ColoredRoundedRectangle extends ColoredRectangle{
+
+
+
+	
+}
---
+++ src/com/OCP81Z0809/QuestionTime/Chap2/Ex5/RoudedRectangle.java
@@ -0,0 +1,5 @@
+package com.OCP81Z0809.QuestionTime.Chap2.Ex5;
+
+public class RoudedRectangle extends Rectangle {
+
+}
