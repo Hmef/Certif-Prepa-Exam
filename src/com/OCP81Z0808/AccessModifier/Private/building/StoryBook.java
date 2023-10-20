4a21f24867291b65b6a7ed0128694abf7d16e042
Hmef <hmeftaah@gmail.com>
2023-10-20 23:27:20 +0200

    private access modifier - delete comment in StoryBook class

--- src/com/OCP81Z0808/AccessModifier/Private/building/StoryBook.java
+++ src/com/OCP81Z0808/AccessModifier/Private/building/StoryBook.java
@@ -3,19 +3,5 @@ package com.OCP81Z0808.AccessModifier.Private.building;
 public class StoryBook {
 
 	
-	
-	
-	
-
-
-/*
- *			**************** ERROR ****************
-  
-  			The classes StoryBook and House don't reside in 
-  			the same package as the class Book
- * 
- * 
- */
-	
 }
 
