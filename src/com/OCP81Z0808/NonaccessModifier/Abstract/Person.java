7093eff9b05329150e894bc5d675e26023c6c049
Hmef <hmeftaah@gmail.com>
2023-10-23 21:53:59 +0200

    Non Access Modifiers - abstract - Compilation Error ==> Person is abstract - cannont instantiate the type of Person - OCP81Z0808

---
+++ src/com/OCP81Z0808/NonaccessModifier/Abstract/Person.java
@@ -0,0 +1,8 @@
+package com.OCP81Z0808.NonaccessModifier.Abstract;
+
+abstract public class Person {
+
+private String name;
+	
+	public void displayName() {};
+}
---
+++ src/com/OCP81Z0808/NonaccessModifier/Abstract/University.java
@@ -0,0 +1,7 @@
+package com.OCP81Z0808.NonaccessModifier.Abstract;
+
+public class University {
+
+	Person person = new Person();  //  cannot instantiate the type of Person ==> abstract class Person
+	
+}
