81555066d8ff8ee7bbe82f9fbdc2893736286c94
Hmef <hmeftaah@gmail.com>
2023-10-26 23:20:22 +0200

    Exam TIP - Identifiers - Identify valid and invalid Identifiers - Syntax Error - Fail to compile - OCP81Z0808

---
+++ src/com/OCP81Z0808/Chap2/Identifiers/Main.java
@@ -0,0 +1,17 @@
+package com.OCP81Z0808.Chap2.Identifiers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		byte exam_total = 7;
+		
+		// int exam-Total = 1090;  // Syntax Error on token "-"
+	}
+	
+	/*
+	 * 		Exam Question : identify a valid and invalid identifiers
+	 * 
+	 * 				==> Identifier : name of packages, classes, interfaces, methods and variables 
+	 */
+}
