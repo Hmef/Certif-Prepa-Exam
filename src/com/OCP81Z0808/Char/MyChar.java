67c89c81ca36b203bdccd5d564ba8711302b2075
Hmef <hmeftaah@gmail.com>
2023-10-26 23:06:26 +0200

    Exam TIP - the question tries to trick you with Complex code that doesn't use any if constructs or do-while loops! - incorrect data type (bool) ==> boolean - fail to compile - OCP81Z0808

--- src/com/OCP81Z0808/Char/MyChar.java
+++ src/com/OCP81Z0808/Char/MyChar.java
@@ -5,7 +5,7 @@ public class MyChar {
 	public static void main(String[] args) {
 		
 		int myInt = 7;
-		boolean result = true;
+		bool result = true;
 		
 		if(result == true) {
 			do
@@ -13,4 +13,13 @@ public class MyChar {
 			while(myInt > 10);
 		}
 	}
+	
+	/*
+	 * 		Exam TIP : The question tries to trick you with Complex code that doesn't use any if constructs or do-while loops !
+	 * 
+	 * 					as we can see, it uses an incorrect data type ==> bool 
+	 * 					to declare and initialize the variable result
+	 * 
+	 * 					==> the code will fail to compile
+	 */
 }
