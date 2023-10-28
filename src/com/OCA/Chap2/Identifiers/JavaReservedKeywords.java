a364128188a7e3f5d45a1843e186843c047f7813
Hmef <hmeftaah@gmail.com>
2023-10-28 22:23:13 +0200

    Exam TIP - Java Reserved Keywords ==> example : volatile - throws - instanceof - transient - const ( not constant or Constant) - OCA

---
+++ src/com/OCA/Chap2/Identifiers/JavaReservedKeywords.java
@@ -0,0 +1,30 @@
+package com.OCA.Chap2.Identifiers;
+
+public class JavaReservedKeywords {
+
+	public static void main(String... args) {
+		
+		
+		//int abstract;  // Java reserved Keyword 
+		
+		int Boolean;  // Valid 
+	
+		
+		/*
+		 * Exam TIP ==> Java Reserved Keywords 
+		 * 
+		 * 
+		 * 			abstract	default		goto	package		this	assert 		do
+		 * 
+		 * 			if		throw		throws		break		import		protected
+		 * 
+		 * 			transient		byte 	enum	short		instanceof		try
+		 * 
+		 * 			extends 	implements		final	volatile	continue
+		 * 
+		 * 			strictfp	const 	finally		native		super 	default
+		 * 
+		 * 			synchronized 	case	null 	for 	private
+		 */
+	}
+}
