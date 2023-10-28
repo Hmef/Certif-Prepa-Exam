318b5d5bd20c3525f176eec6b14ed8bf11042b34
Hmef <hmeftaah@gmail.com>
2023-10-28 22:46:01 +0200

    Practice Tests - Chap1 - Java Basics - Ex12 - Variable scope - static method - non-static variable - instance of the class - Compilation Error - OCA

--- src/com/OCA/PracticeTests/Chap1/JavaBasics/Ex12/ParkRanger.java
+++ src/com/OCA/PracticeTests/Chap1/JavaBasics/Ex12/ParkRanger.java
@@ -8,7 +8,21 @@ public class ParkRanger {
 		
 		int trees = 5;
 		
-		System.out.println(trees + birds);
+		System.out.println(trees + birds);  // Compilation 
 		
 	}
+	
+	/*
+	 * 		Variable scope  
+	 * 		
+	 * 		static method ==> main method is a static method 
+	 * 
+	 * 		not static variable ==> birds
+	 * 
+	 * 		class instance variable ==> parkRanger ( ParkRanger parkRanger = new ParkRanger())
+	 */
+	
+	/*
+	 * 		Compilation Error ==> when a static method (main) try to access to a non-static variable (trees) without an instance of the class
+	 */
 }
