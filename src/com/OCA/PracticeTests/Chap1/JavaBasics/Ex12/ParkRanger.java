eff30e44ba2a8f1c3fefaa3216d607e35da0e79a
Hmef <hmeftaah@gmail.com>
2023-10-28 22:35:45 +0200

    Practice Tests - Chap1 - Java Basics - Ex 12 - OCA

---
+++ src/com/OCA/PracticeTests/Chap1/JavaBasics/Ex12/ParkRanger.java
@@ -0,0 +1,14 @@
+package com.OCA.PracticeTests.Chap1.JavaBasics.Ex12;
+
+public class ParkRanger {
+
+	int birds = 10;
+	
+	public static void main(String[] data) {
+		
+		int trees = 5;
+		
+		System.out.println(trees + birds);
+		
+	}
+}
