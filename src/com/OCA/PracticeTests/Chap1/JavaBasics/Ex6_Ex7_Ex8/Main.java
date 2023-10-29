6a138c5c69c62c70cd78388254f7d21a6a05ff48
Hmef <hmeftaah@gmail.com>
2023-10-29 18:38:56 +0100

    Practice Tests - Chap 1 - Java Basics - Ex6-EX7-Ex8 -Scope of a local variables - package imported into every Java class by Default - Valid comments - OCA

---
+++ src/com/OCA/PracticeTests/Chap1/JavaBasics/Ex6_Ex7_Ex8/Main.java
@@ -0,0 +1,42 @@
+package com.OCA.PracticeTests.Chap1.JavaBasics.Ex6_Ex7_Ex8;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 			Exercice 6 ==> Scope of a Local variable
+		 * 
+		 * 						==> Local variables have a scope LiMited to a method 
+		 * 
+		 * 							==> Only Local variables have such a small scope 
+		 */
+		
+
+		/*
+		 * 			Exercice 7 ==> package imported into every java class by default 
+		 * 
+		 * 							==> java.lang 
+		 * 
+		 * 		the other class most be explicitly imported ( java.util )
+		 * 			
+		 * 		system.lang and java.system do not exist in the standard Java runtime
+		 * 
+		 */
+		
+
+		/*
+		 * 			Exercice  8
+		 * 
+		 * 			# Add Configuration Value ==> is not a valid Comment 
+		 */
+		
+		// add to result 5 
+		
+		/*** TODO: Fix Bug 12312 ***/
+		
+		//  ==> # Add Configuration Value ==> is not a valid Comment 
+		
+		/* Read file from system ***/
+	}
+}
