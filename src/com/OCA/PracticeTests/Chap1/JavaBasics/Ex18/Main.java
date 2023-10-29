d362d3ded94f38e3bd6608006e79f0d0d27dd272
Hmef <hmeftaah@gmail.com>
2023-10-29 20:38:33 +0100

    Practice tests - Chap1 - Ex18 - Initialize the variable height - Solution - equal() method    Vs   == - OCA

--- src/com/OCA/PracticeTests/Chap1/JavaBasics/Ex18/Main.java
+++ src/com/OCA/PracticeTests/Chap1/JavaBasics/Ex18/Main.java
@@ -6,12 +6,20 @@ public class Main {
 		
 		String tree = "pine";
 		int count = 0;
-		
+		 
+		int height = 0;  // Solution 
 		if(tree.equals("pine")) {
 			
-			int height = 50; // Local variable
+			//int height = 50; // Local variable
+			height = 50;
 			count = count + 1;
 		}	
-		System.out.println(height + count); // Compilation Error 
+		System.out.println(height + count); // not compile ==> Initialize variable ==> int height = 0;
 	}
+	
+	/*
+	 * 		the height variable is declared within the if statement block
+	 * 
+	 * 		it cannot be referenced outside the if statement and the code does not compile 
+	 */
 }
