31c77bbf6afa5b5586ada07691f4c90652c74648
Hmef <hmeftaah@gmail.com>
2023-10-31 22:01:43 +0100

    Practice Test - Chap4 - Ex8 - Sorted Array ==> Arrays.sort(days) - OCA

--- src/com/OCA/PracticeTests/Chap4/Arrays/Ex8/Main.java
+++ src/com/OCA/PracticeTests/Chap4/Arrays/Ex8/Main.java
@@ -1,5 +1,7 @@
 package com.OCA.PracticeTests.Chap4.Arrays.Ex8;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,5 +16,15 @@ public class Main {
 			System.out.println(" i ==> " + i);
 			System.out.println(days[i]);
 		}
+		
+		System.out.println("");
+		
+		Arrays.sort(days);
+		//System.out.println(days.toString()); 
+		
+		System.out.print("Sorted Array ==> ");
+		System.out.println(Arrays.toString(days));
+		
+		
 	}
 }
