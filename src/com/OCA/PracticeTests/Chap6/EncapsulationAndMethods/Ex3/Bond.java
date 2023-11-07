12947f9aab724fc965c05b80b58bb67b66ab1e4c
Hmef <hmeftaah@gmail.com>
2023-11-07 23:42:27 +0100

    Practice tests - Chap6 - Methods & Encapsulation - if-else if - return boolean value - Doesn't Compile - OCA

---
+++ src/com/OCA/PracticeTests/Chap6/EncapsulationAndMethods/Ex3/Bond.java
@@ -0,0 +1,27 @@
+package com.OCA.PracticeTests.Chap6.EncapsulationAndMethods.Ex3;
+
+public class Bond {
+
+	public static int price = 5;
+	
+	public boolean sell() {
+		
+		if(price<10) {
+			price++;
+			return true;	
+		} else if(price>=10) {
+			return false;
+		}
+		
+		//return true;
+	}
+	
+	public static void main(String[] args) {
+		
+		new Bond().sell();
+		new Bond().sell();
+		new Bond().sell();
+		
+		System.out.println(price);
+	}
+}
