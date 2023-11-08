bd371104564a66aaa440a234aa2e41e686cbba2e
Hmef <hmeftaah@gmail.com>
2023-11-08 21:44:30 +0100

    Practice Tests - Chap6 - Methods & Encapsulation - Ex3 - Increment - if-else - static - Test of Increment value - OCA

--- src/com/OCA/PracticeTests/Chap6/EncapsulationAndMethods/Ex3/Bond.java
+++ src/com/OCA/PracticeTests/Chap6/EncapsulationAndMethods/Ex3/Bond.java
@@ -2,18 +2,22 @@ package com.OCA.PracticeTests.Chap6.EncapsulationAndMethods.Ex3;
 
 public class Bond {
 
-	public static int price = 5;
+	public static int price = 5;  // static to call its in the static main method 
 	
 	public boolean sell() {
 		
 		if(price<10) {
 			price++;
+			System.out.println("if price < 10 :  " + price);
+			
 			return true;	
 		} else if(price>=10) {
+			
+			System.out.println(" else if price >= 10 : " + price);
 			return false;
 		}
 		
-		//return true;
+		//return true;  // 
 	}
 	
 	public static void main(String[] args) {
@@ -22,6 +26,12 @@ public class Bond {
 		new Bond().sell();
 		new Bond().sell();
 		
-		System.out.println(price);
+		// test of Increment value 
+		new Bond().sell();
+		new Bond().sell();
+		new Bond().sell();
+		new Bond().sell();
+		
+		System.out.println(price);  // if prince is not static we can't call it here 
 	}
 }
