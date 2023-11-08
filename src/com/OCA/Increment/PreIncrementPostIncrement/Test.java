7f66e7319a591b5616ac1121c3a5d96adb6b3c56
Hmef <hmeftaah@gmail.com>
2023-11-08 23:38:01 +0100

    Exam TIP - Post-Increment ==> we make the operation with the actual value and then x is Incremented - OCA

--- src/com/OCA/Increment/PreIncrementPostIncrement/Test.java
+++ src/com/OCA/Increment/PreIncrementPostIncrement/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		
 		int[] arr = {2, 5, 6, 9, 4};
 		
+		// Pre-Increment in a loop 
 		for(int i=0; i<arr.length; ++i) {  
 			
 			System.out.print(arr[i] + " ");  // i == 0 not i == 1
@@ -20,7 +21,12 @@ public class Test {
 		int b = 1;
 		System.out.println(" ++b : " + ++b); 
 		
-		int c = 10++; // we can't increment a constant value 
+		//int c = 10++; // ==> Error - we can't increment a constant value 
+		
+		int x = 4;
+		int y = x++ + 100;  // y == 104   x == 5 ( we make the operation with the actual value of x and then x is Incremented )
+		System.out.println(" x : " + x);
+		System.out.println("y : " + y);
 		
 		/*
 		 * 							Exam TIP 
