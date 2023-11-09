cae91ceb6191d7a98ab5c103b427142f7d5c518e
Hmef <hmeftaah@gmail.com>
2023-11-09 23:39:59 +0100

    Practice Tests - Chap2 - Solution - Change to static - Integer.valueOf(int) - ex9 - Data Types - OCA

--- src/com/OCA/PracticeTests/Chap2/DataTypes/Ex9/Values.java
+++ src/com/OCA/PracticeTests/Chap2/DataTypes/Ex9/Values.java
@@ -2,13 +2,18 @@ package com.OCA.PracticeTests.Chap2.DataTypes.Ex9;
 
 public class Values {
 
-	Integer a = Integer.valueOf(1);  // Error ==< the code does not compile
+	Integer a = Integer.valueOf(1);  // Error ==> the code does not compile
+	static int m = 55;  // Error ==> change to static 
 	
 	public static void main(String[] args) {
 		
-		Integer a = Integer.valueOf(2);  // Error  ==> there is no class named integer 
-		Integer b = Integer.valueOf(3);  // Error
+		
+		Integer a = Integer.valueOf(2);  // Solution  ==> there is no class named integer ==> Integer is a class 
+		Integer b = Integer.valueOf(3);  // int is a primitive type and Integer is a class 
+		
+		Integer n = Integer.valueOf(m);
 		
 		System.out.println(a + b);
+		System.out.println(n);
 	}
 }
