d69aa17770da4117d863d724c940e3ad9ef6da76
Hmef <hmeftaah@gmail.com>
2023-11-09 01:03:33 +0100

    Practice Tests - Chap6 - Methods & Encapsulation - Ex10 - Exam TIP - this(4)   Vs    new Jump(4) - instance is Nested - Default Initialization of a boolean Instance variable is ==< false - static attribute - OCA

--- src/com/OCA/PracticeTests/Chap6/EncapsulationAndMethods/Ex10/Jump.java
+++ src/com/OCA/PracticeTests/Chap6/EncapsulationAndMethods/Ex10/Jump.java
@@ -8,17 +8,27 @@ public class Jump {
 	public Jump() {
 		
 		// p1 ==> which line of code, inserted at line p1, causes the application to print 5
+		this(4);
+		// new Jump(4); // that instance is Nested 
 		outside = true;
 	}
 	
 	public Jump(int rope) {
 		
-		this.rope = outside ? rope : rope+1;
+		this.rope = outside ? rope : rope+1;  // outside ==> is false or true ??
+		
+		//System.out.println(" outside : " + outside);
 	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println(new Jump().rope);
+		System.out.println(" new Jump().rope ==> " + new Jump().rope);  
+		
+		//System.out.println(rope); // Error it's not a static attribute 
+	
+		System.out.println(" new Jump() ==> " + new Jump().outside);
+		System.out.println(" new Jump(4).rope ==>  " + new Jump(4).rope);
+		System.out.println(" new Jump(5).outside ==> " + new Jump(5).outside);
 	}
 	
 	/*
@@ -28,4 +38,21 @@ public class Jump {
 	 * 		D. rope = 4;
 	 * 
 	 */
+	
+	/*
+	 * 					Exam TIP
+	 * 
+	 * 		Remember that the Default initialization of a boolean INSTANCE variable is ==> false 
+	 * 
+	 */
+	
+	/*
+	 * 
+	 * 					Exam TIP 
+	 * 
+	 * 		B. new Jump(4) ==> that instance is Nested and the value of rope does not affect the surrounding instance of Jump 
+	 * 						that the constructor was called in 
+	 */
+	
+	
 }
