c540abb7cc97fadf37a0e791dd9424780308c7c1
Hmef <hmeftaah@gmail.com>
2023-11-09 23:32:40 +0100

    Practice Tests - Solution ==> Integer is a class not integer - there is a primitive int - Integer.valueOf(2) - OCA

--- src/com/OCA/PracticeTests/Chap2/DataTypes/Ex9/Values.java
+++ src/com/OCA/PracticeTests/Chap2/DataTypes/Ex9/Values.java
@@ -2,12 +2,12 @@ package com.OCA.PracticeTests.Chap2.DataTypes.Ex9;
 
 public class Values {
 
-	integer a = Integer.valueOf(1);  // Error
+	Integer a = Integer.valueOf(1);  // Error ==< the code does not compile
 	
 	public static void main(String[] args) {
 		
-		integer a = Integer.valueOf(2);  // Error  ==> there is no class named integer 
-		integer b = Integer.valueOf(3);  // Error
+		Integer a = Integer.valueOf(2);  // Error  ==> there is no class named integer 
+		Integer b = Integer.valueOf(3);  // Error
 		
 		System.out.println(a + b);
 	}
