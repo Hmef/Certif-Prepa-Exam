9b9d012264fbf3735a43c871740df2ccfd79c74d
Hmef <hmeftaah@gmail.com>
2023-11-10 18:20:20 +0100

    Practice Tests - Chap2 - Data Types - Ex29 - Constructor - Instance - new - OCA

---
+++ src/com/OCA/PracticeTests/Chap2/DataTypes/Ex29/TennisBall.java
@@ -0,0 +1,16 @@
+package com.OCA.PracticeTests.Chap2.DataTypes.Ex29;
+
+public class TennisBall {
+
+	public TennisBall() {
+		
+		System.out.println("bounce");
+	}
+	
+	public static void main(String[] args) {
+		
+		new TennisBall();
+		
+	}
+	
+}
