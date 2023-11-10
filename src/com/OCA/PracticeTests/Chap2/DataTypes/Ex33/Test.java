9b378040e8d0ce20c8108e22a9e3e1b2e21b7951
Hmef <hmeftaah@gmail.com>
2023-11-10 20:05:24 +0100

    Practice Tests - Chap2 - Data Types - Ex33 - Which statement is true about primitives? - lowercase letter - create your own primitive type ?? ==> Non  - OCA

---
+++ src/com/OCA/PracticeTests/Chap2/DataTypes/Ex33/Test.java
@@ -0,0 +1,32 @@
+package com.OCA.PracticeTests.Chap2.DataTypes.Ex33;
+
+public class Test {
+
+	
+	/*
+	 * 		Which statement is true about primitives ?
+	 * 
+	 * 		A. Primitive types begin with a lowercase letter 
+	 * 
+	 * 		B. Primitive types can be set to null
+	 * 
+	 * 		C. String is a primitive
+	 * 
+	 * 		D. You can create your own primitive type 	
+	 */
+	
+	
+	/*
+	 * 					Exam TIP
+	 * 
+	 * 		- All the primitive types are lowercase 
+	 * 
+	 * 		- Primitives cannot reference null, unlike object reference variables 
+	 * 
+	 * 		- String is not a primitive as evidenced by the uppercase letter in the name	
+	 * 		
+	 * 		- You can create your own classes but not primitives
+	 * 
+	 */
+}
+
