dfef151868ba7366b8f61dfcd9d175fe169b63bb
Hmef <hmeftaah@gmail.com>
2023-11-10 19:49:59 +0100

    Practice Tests - Chap2 - Data types - Ex31 - Wrapper classes - two primitives have wrapper classes that are not merely the name of the primitive with an Uppercase letter ==> int and char ==> the wrapper class of int ==> Integer - the wrapper class of char is ==> Character - OCA

---
+++ src/com/OCA/PracticeTests/Chap2/DataTypes/Ex31/WrapperClassTest.java
@@ -0,0 +1,27 @@
+package com.OCA.PracticeTests.Chap2.DataTypes.Ex31;
+
+public class WrapperClassTest {
+
+	/*
+	 * 			Which two primitives have wrappers classes that are not merely 
+	 * 			the name of the primitive with an uppercase letter 
+	 * 
+	 * 		A. byte and char
+	 * 		B. byte and int 
+	 * 		C. char and int 
+	 * 		D. None of the above 
+	 * 
+	 */
+	
+	
+	/*
+	 * 
+	 *  	The wrapper class of ==> int is Integer 
+	 *  
+	 *  	The wrapper class of ==> char is Character 
+	 *  
+	 *  	all other primitives have the same name 
+	 *  		==> Example ==> the wrapper class of boolean is Boolean 
+	 * 	
+	 */
+}
