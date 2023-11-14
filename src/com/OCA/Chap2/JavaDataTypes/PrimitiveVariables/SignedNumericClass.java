f44306c07df5599597fa4bb3f8f429b6a067cf78
Hmef <hmeftaah@gmail.com>
2023-11-14 22:39:25 +0100

    Chap2 - Signed Numeric - Integers ==> byte - short - int - long - Signed Numeric ( store positive and negative integers - Exam TIP - OCA

---
+++ src/com/OCA/Chap2/JavaDataTypes/PrimitiveVariables/SignedNumericClass.java
@@ -0,0 +1,29 @@
+package com.OCA.Chap2.JavaDataTypes.PrimitiveVariables;
+
+public class SignedNumericClass {
+
+	public static void main(String[] args) {
+		
+		byte num = 100;
+		short sum = 1240;
+		int total = 48764;
+		long population = 214748368;
+		
+		long fishInSea = 764398609800L;
+		
+		/*
+		 * 		
+		 * 			signed numeric ==> Integers ==> byte, short, int, long 
+		 * 
+		 * 								byte : -128 - 127			
+		 */
+		
+		/*
+		 * 						Exam TIP
+		 * 
+		 * 			char is an unsigned numeric data type 
+		 * 
+		 * 			==> it can only store a Positive integers
+		 */
+	}
+}
