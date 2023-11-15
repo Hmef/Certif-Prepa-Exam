52ce73cd8c6a204d12f996aecf4feeef4efefbc9
Hmef <hmeftaah@gmail.com>
2023-11-15 23:51:09 +0100

    Practice Test - Chap5 - loops - Ex20 - List - Arrays.asList() - for loop - list.get(container) - OCA

---
+++ src/com/OCA/PracticeTests/Chap5/loop/Ex20/Test.java
@@ -0,0 +1,18 @@
+package com.OCA.PracticeTests.Chap5.loop.Ex20;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		List<String> drinks = Arrays.asList("can","cup");
+		
+		for(int container = 0; container < drinks.size(); container++) {
+			
+			System.out.println(drinks.get(container) + ",");
+		}
+	}
+
+}
