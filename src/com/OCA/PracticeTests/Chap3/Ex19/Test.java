3b4e3b72858a9f2ff4eb62b0cef09b3b5b68a571
Hmef <hmeftaah@gmail.com>
2023-11-18 03:50:12 +0100

    Embedded ternary Operations - boolean ==> solution - Practice Tests - Chap3 - Ex19 - OCA

--- src/com/OCA/PracticeTests/Chap3/Ex19/Test.java
+++ src/com/OCA/PracticeTests/Chap3/Ex19/Test.java
@@ -5,9 +5,17 @@ public class Test {
 	public static void main(String[] args) {
 		
 		int time = 11;
-		int day = 4;
+		//int day = 4;
+		boolean day = true;
 		
 		String dinner = time > 10 ? day ? "Takeout" : "Salad" : "Leftovers"; //Error==> day is not a booealn
+		System.out.println(dinner);
+		
+		/*
+		 or 
+		 int day = 4;
+		 String dinner = time > 10 ? day < 5 ? "Takeout" : "Salad" : "Leftovers";
+		 */
 		
 	}
 }
