0f67fd395c9e2af3f405aabfe9c94a3889c3ba1c
Hmef <hmeftaah@gmail.com>
2023-11-18 03:40:49 +0100

    Ternary Operator - Practice tests - Chap3 - Ex19 - Error ==> day is not a boolean - OCA

---
+++ src/com/OCA/PracticeTests/Chap3/Ex19/Test.java
@@ -0,0 +1,13 @@
+package com.OCA.PracticeTests.Chap3.Ex19;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int time = 11;
+		int day = 4;
+		
+		String dinner = time > 10 ? day ? "Takeout" : "Salad" : "Leftovers"; //Error==> day is not a booealn
+		
+	}
+}
